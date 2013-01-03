a3b83edf61a7b2a45650e431c493a66b9012ded4
thunder9 <livebythunder@yahoo.co.jp>
2013-01-03 15:08:41 +0900

    Allow Image/Canvas/VideoElement as option value

--- textgram.js
+++ textgram.js
@@ -7,7 +7,7 @@
             
   $.fn.textgram = function(options) {
     var $containers = this,
-        imagedata   = null,
+        image       = null,
         offsetX     = 0,
         offsetY     = 0,
         width       = 0,
@@ -16,7 +16,7 @@
         repeatY     = false;
 
     if ($.isPlainObject(options)) {
-      imagedata = options.imagedata;
+      image = options.image || null;
       offsetX = options.x || 0;
       offsetY = options.y || 0;
       width = options.width || 0;
@@ -24,12 +24,22 @@
       repeatX = options.repeatX || false;
       repeatY = options.repeatY || false;
     } else {
-      imagedata = options;
+      image = options;
     }
     
-    if (imagedata) {
-      width = width || imagedata.width;
-      height = height || imagedata.height;       
+    if (!image) return false;  
+      
+    if ('width' in image && 'height' in image) {
+      width = width || image.width;
+      height = height || image.height;       
+    }
+    if (!('data' in image)) {
+      var canvas = document.createElement('canvas'),
+          context = canvas.getContext('2d');
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(image, 0, 0, width, height);
+      image = context.getImageData(0, 0, width, height);
     }
       
     return $containers.each(function() {
@@ -56,20 +66,20 @@
               h   = $ch.height(),
               x   = (pos.left + 0.5 * w - offsetX) / width,
               y   = (pos.top + 0.5 * h - offsetY) / height;
-          w *= imagedata.width / width;
-          h *= imagedata.height / height;
+          w *= image.width / width;
+          h *= image.height / height;
           if (x >= 0 && y >= 0 && (repeatX || x <= 1) && (repeatY || y <= 1)) { 
             var rgba    = [0, 0, 0, 0],
                 counted = 0;
-            x = (x - Math.floor(x)) * imagedata.width;
-            y = (y - Math.floor(y)) * imagedata.height;
+            x = (x - Math.floor(x)) * image.width;
+            y = (y - Math.floor(y)) * image.height;
             for (var s = 0; s < w; s++) {
               for (var t = 0; t < h; t++) {
                 var u   = Math.round(x + s - 0.5 * w),
                     v   = Math.round(y + t - 0.5 * h),
-                    pos = u + v * imagedata.width;
+                    pos = u + v * image.width;
                 for (var i = 0; i < 4; i++) {
-                  rgba[i] += imagedata.data[pos * 4 + i];
+                  rgba[i] += image.data[pos * 4 + i];
                 }
                 counted++;
               }  
